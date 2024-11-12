1c5e93975f8b65c4bb7f1d4b36b306c33be711fb
Eliaslopes248 <ealopes@aggies.ncat.edu>
2024-11-11 21:19:58 -0500

    set up body parser and static rendering

--- project/backend/server.js
+++ project/backend/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const fs = require("fs")
-const bodyparser = require('body-parse')
+const bodyparser = require('body-parser')
 const path = require("path")
 
 const port = 3000
 
 
 const app = express()
+
+app.use(express.static('../',__dirname,'frontend'))
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
 
 
 
