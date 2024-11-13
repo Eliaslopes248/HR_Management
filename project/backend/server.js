afbb7ab34980d6b1b7bd6b4d7ad7fb2c0f72ed1a
Eliaslopes248 <ealopes@aggies.ncat.edu>
2024-11-12 21:33:54 -0500

    login and sign up created

--- project/backend/server.js
+++ project/backend/server.js
@@ -3,19 +3,27 @@ const fs = require("fs")
 const bodyparser = require('body-parser')
 const path = require("path")
 
+// port running server
 const port = 3000
-
-
+// app instance
 const app = express()
 
-app.use(express.static('../',__dirname,'frontend'))
+//render static code from frontend directory
+app.use(express.static(path.join(__dirname,'../frontend')))
 
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended:true}))
 
 
 
+//index route
+app.get("/",(req,res)=>{
+    res.redirect('/index.html')
+})
 
+
+
+//app listening
 app.listen(port,()=>{
     console.log("Backend Connected!")
 })
